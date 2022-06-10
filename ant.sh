b0be95d98efdd77a8e96065f19f6524e73cef474
Alice Jacka <alice@jacka.net.au>
2022-06-10 19:09:44 +1000

    cleanup un-needed files

--- ant.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export PATH=/usr/lib/jvm/oracle-java8-jdk-amd64/bin:$PATH
-export JAVA_HOME=/usr/lib/jvm/oracle-java8-jdk-amd64
-export JAVACARD_HOME=/home/user/Exports/javacard-sdk/jc222_kit/
-ant -DJAVACARD_HOME=$JAVACARD_HOME $@
