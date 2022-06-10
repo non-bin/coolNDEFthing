b7c0e2fdcf10ff8b60015a59beba44e3782edd4f
Alice Jacka <alice@jacka.net.au>
2022-06-10 19:08:36 +1000

    change class name and simplify build.xml

--- src/main/java/au/net/jacka/coolNDEFthing/NdefApplet.java
+++ src/main/java/au/net/jacka/coolNDEFthing/NdefApplet.java
@@ -1,23 +1,22 @@
 /*
- * openjavacard-ndef: JavaCard applet implementing an NDEF tag
- * Copyright (C) 2015-2018 Ingo Albrecht <copyright@promovicz.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+    coolNDEFthing - a javacard applet that provides services through NDEF
+    Copyright (C) 2022  non-bin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.openjavacard.ndef.advanced;
+package au.net.jacka.coolNDEFthing;
 
 import javacard.framework.APDU;
 import javacard.framework.Applet;
